a58c65ef0d96e1a8136fab172a8dc0bdd34d6491
Jörgen Karlsson <mr.jorgen@bredband.net>
2018-03-26 18:56:56 +0200

    Continue working on Tidrapport

--- public/tidrapport/main.js
+++ public/tidrapport/main.js
@@ -1,11 +1,38 @@
 document.addEventListener("DOMContentLoaded", (e) => {
-  document.querySelector("#weekNrInput").value = new Date().getWeek();
-})
+  let weekNr = document.querySelector("#weekNrInput"),
+    dateStart = document.querySelector("#dateStartInput"),
+    monthStart = document.querySelector("#monthStartInput"),
+    dateEnd = document.querySelector("#dateEndInput"),
+    monthEnd = document.querySelector("#monthEndInput"),
+    year = document.querySelector("#yearInput");
+  
+  weekNr.value = new Date().getWeek();
+  
+  let firstDayOfWeek = getFirstDayOfWeek();
+  dateStart.value = firstDayOfWeek.getDate();
+  monthStart.value = firstDayOfWeek.getMonth() + 1;
+
+  let lastDayOfWeek = new Date();
+  lastDayOfWeek.setDate(firstDayOfWeek.getDate() + 6);
+  dateEnd.value = lastDayOfWeek.getDate();
+  monthEnd.value = lastDayOfWeek.getMonth() + 1;
+  
+  year.value = parseInt(firstDayOfWeek.getFullYear()) - 2000;
+
+});
+
+function getFirstDayOfWeek() {
+  let tmpDate = new Date();
+  while (tmpDate.getDay() !== 0) {
+    tmpDate.setDate(tmpDate.getDate() - 1);
+  }
+  return tmpDate;
+}
 
 
 
 // Returns the ISO week of the date.
-Date.prototype.getWeek = function () {
+Date.prototype.getWeek = function() {
   let date = new Date(this.getTime());
   date.setHours(0, 0, 0, 0);
   // Thursday in current week decides the year.
@@ -13,13 +40,11 @@ Date.prototype.getWeek = function () {
   // January 4 is always in week 1.
   let week1 = new Date(date.getFullYear(), 0, 4);
   // Adjust to Thursday in week 1 and count number of weeks from date to week1.
-  return (
-    1 +
-    Math.round(
-      ((date.getTime() - week1.getTime()) / 86400000 -
-        3 +
-        (week1.getDay() + 6) % 7) /
-      7
-    )
-  );
+  return (1 + Math.round(((date.getTime() - week1.getTime()) / 86400000 - 3 + (week1.getDay() + 6) % 7) / 7));
 };
+
+Date.prototype.addDays = function(days) {
+  var dat = new Date(this.valueOf());
+  dat.setDate(dat.getDate() + days);
+  return dat;
+}
