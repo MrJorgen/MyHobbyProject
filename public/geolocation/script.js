530b2078a61d51e5d652c7923fd2c9fbd38e64b2
Jörgen Karlsson <mr.jorgen@bredband.net>
2021-01-31 20:09:30 +0100

    Debug

--- public/geolocation/script.js
+++ public/geolocation/script.js
@@ -1,10 +1,22 @@
-let speedEle = document.querySelector("#speed");
+let speedEle = document.querySelector("#speed"), debug = true;
 
-let options = {
+const options = {
     enableHighAccuracy: true,
     timeout: 1000,
     maximumAge: 0
   };
+
+  const errors = {
+      noSpeed: {
+          error: false,
+          text: "<li>No speed data</li>"
+      },
+      lowAccuracy: {
+          error: false,
+          text: "<li>Low accuracy</li>",
+          accuracy: null
+      }
+  }
 
 speedEle.textContent = "test";
 
@@ -19,24 +31,36 @@ else {
 navigator.geolocation.getCurrentPosition(onSuccess, onError, options);
 
 function onSuccess(pos) {
+    document.querySelector("#error").innerHTM = "";
     console.log(pos);
-    let error = "";
+
+    let errorMessage = "", error = false;
+
     speedEle.style.fontSize = "50px";
     if(pos.coords.speed === null) {
-        error += "<li>No speed data</li>";
+        errors.noSpeed.error = true;
+        errorMessage += errors.noSpeed.text;
+        error = true;
     }
 
     if(pos.coords.accuracy > 50) {
-        error += "<li>Low accuracy</li>";
+        errors.lowAccuracy.error = true;
+        error = true;
+        errorMessage += errors.lowAccuracy.text;
     }
-    else {
-        error += `<li>Accuracy: ${pos.coords.accuracy.toFixed(1)} m</li>`;
+
+    if(!error) {
+        errorMessage += `<li>Accuracy: ${pos.coords.accuracy.toFixed(1)} m</li>`;
         let kmH = pos.coords.speed * 1000 / 3600;
         speedEle.textContent = Math.round(kmH) + " km/h";
     }
-
-    if(error.length > 0) {
-        lowAccuracy(error);
+    else {
+        document.querySelector("#error").innerHTML = errorMessage;
+    }
+    if(debug) {
+        document.querySelector("#info").innerHTML += `Accuracy: ${pos.coords.accuracy}<br>`;
+        document.querySelector("#info").innerHTML += `Speed: ${pos.coords.speed}<br>`;
+        document.querySelector("#info").innerHTML += `Age: ${Date.now() - pos.timestamp} ms`;
     }
 }
 
@@ -45,8 +69,4 @@ function onError(error) {
     console.log(error.message);
     speedEle.style.fontSize = "25px";
     speedEle.textContent = error.message;
-  }
-
-  function lowAccuracy(error) {
-    document.querySelector("#error").innerHTML = error;
   }
