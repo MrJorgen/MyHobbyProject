dce0f1e6dca0d2db566e783609bf9e50f8fa4527
Jörgen Karlsson <mr.jorgen@bredband.net>
2021-02-03 16:52:36 +0100

    Cosmetic changes

--- public/speedometer/script.js
+++ public/speedometer/script.js
@@ -21,27 +21,26 @@ const errors = {
 
 const accuracyRange = [
   {
-    color: "hsl(80, 100%, 50%)",
-    value: 10,
-    width: 1
+    value: 8,
+    cssClass: "bar-high"
   },
   {
-    color: "hsl(60, 100%, 50%)",
+    value: 12,
+    cssClass: "bar-mid-high"
+  },
+  {
     value: 20,
-    width: 0.75
+    cssClass: "bar-mid-low"
   },
   {
-    color: "hsl(40, 100%, 50%)",
-    value: 50,
-    width: 0.5
+    value: 100,
+    cssClass: "bar-low"
+  },
+  {
+    value: Infinity,
+    cssClass: "bar-none"
   }
 ];
-
-/*
-HUE Color range
-from Red to Green
-0 - 120
-*/
 
 if (navigator.geolocation) {
   console.log("Geolocation supported");
@@ -52,19 +51,15 @@ if (navigator.geolocation) {
 
 function onSuccess(pos) {
   document.querySelector("#error").innerHTM = "";
-  console.log(pos);
-  let accuracyColor = "hsl(0, 100%, 50%)";
 
-  let bar = document.querySelector("#bar");
+  let bar = document.querySelector("#accuracy-bar");
   for(let i = 0; i < accuracyRange.length; i++) {
     if(pos.coords.accuracy < accuracyRange[i].value) {
-      accuracyColor = accuracyRange[i].color;
-      bar.style.width = accuracyRange[i].width * 100 + "%";
-      bar.style.backgroundColor = accuracyRange[i].color;
+      bar.className = "";
+      bar.classList.add(accuracyRange[i].cssClass);
       break;
     }
   }
-  console.log(accuracyColor);
 
   let errorMessage = "",
     error = false;
@@ -91,7 +86,7 @@ function onSuccess(pos) {
   if (debug) {
     document.querySelector("#info").innerHTML = "";
     document.querySelector("#info").innerHTML += `Accuracy: ${pos.coords.accuracy.toFixed(1)} m<br>`;
-    document.querySelector("#info").innerHTML += `Speed: ${pos.coords.speed} m/s<br>`;
+    document.querySelector("#info").innerHTML += "Speed:" + (pos.coords.speed === null) ? "No measurment<br>" : pos.coords.speed.toFixed(2) + "m/s<br>";
     document.querySelector("#info").innerHTML += `Age: ${Date.now() - pos.timestamp} ms`;
   }
 }
