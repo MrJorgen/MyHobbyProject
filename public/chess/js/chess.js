f397ab2b48a79bfc95d5aa1c53276f2dd9151e86
Jörgen Karlsson <mr.jorgen@bredband.net>
2021-04-17 22:44:28 +0200

    Chess update

--- public/chess/js/chess.js
+++ public/chess/js/chess.js
@@ -1,17 +1,24 @@
 import { ChessPiece, black, white, ChessBoard } from "./classes.js";
 
 const canvas = document.querySelector("#bgCanvas"),
-  ctx = canvas.getContext("2d"),
+  bgCtx = canvas.getContext("2d"),
   canvas2 = document.querySelector("#layerCanvas"),
-  ctx2 = canvas2.getContext("2d"),
+  guideCtx = canvas2.getContext("2d"),
   canvas3 = document.querySelector("#piecesCanvas"),
-  ctx3 = canvas3.getContext("2d"),
-  colors = {
+  piecesCtx = canvas3.getContext("2d"),
+  animCanvas = document.querySelector("#animCanvas"),
+  animCtx = animCanvas.getContext("2d"),
+  themes = {
     line: "rgb(50, 50, 50)",
     silver: {
       black: "#b0b2b5",
       white: "#e5e5e6",
       padding: "#cdcdd0",
+    },
+    test: {
+      black: "#b58863", // (181,136,99)
+      white: "#f0d9b5", // (240,217,181)
+      padding: "rgb(211, 176, 141)",
     },
     brown: {
       black: "#b2936c",
@@ -20,7 +27,7 @@ const canvas = document.querySelector("#bgCanvas"),
     },
   };
 
-const theme = "silver";
+const theme = "test";
 
 let mouse = {
   x: 0,
@@ -30,6 +37,7 @@ let mouse = {
 
 let size = parseInt(Math.min(window.innerWidth, window.innerHeight) * 0.75);
 size = size - (size % 9);
+// size = 1200;
 const squareSize = size / 9,
   startPos = squareSize / 2 - 0.5;
 
@@ -39,66 +47,68 @@ canvas2.width = size;
 canvas2.height = size;
 canvas3.width = size;
 canvas3.height = size;
+animCanvas.width = size;
+animCanvas.height = size;
 
 const board = new ChessBoard();
 setup();
 
 async function setup() {
-  for (let piece in black) {
-    black[piece].img = await loadImage(black[piece].imgName);
-    board.pieces[black[piece].x][black[piece].y] = new ChessPiece(piece, "black", black[piece].img, black[piece].x, black[piece].y);
+  for (let piece of black) {
+    piece.img = await loadImage(piece.imgName);
+    board.pieces[piece.x][piece.y] = new ChessPiece(piece.type, "black", piece.img, piece.x, piece.y);
   }
 
-  for (let piece in white) {
-    white[piece].img = await loadImage(white[piece].imgName);
-    board.pieces[white[piece].x][white[piece].y] = new ChessPiece(piece, "white", white[piece].img, white[piece].x, white[piece].y);
+  for (let piece of white) {
+    piece.img = await loadImage(piece.imgName);
+    board.pieces[piece.x][piece.y] = new ChessPiece(piece.type, "white", piece.img, piece.x, piece.y);
   }
-
+  console.log(board);
   drawBoard();
   drawPieces();
 }
 
 function drawPieces() {
-  ctx3.clearRect(0, 0, size, size);
-  for (let x of board.pieces) {
-    for (let piece of x) {
+  piecesCtx.clearRect(0, 0, size, size);
+  for (let tmp of board.pieces) {
+    for (let piece of tmp) {
       if (piece) {
-        ctx3.drawImage(piece.img, piece.x * squareSize + startPos, piece.y * squareSize + startPos, squareSize, squareSize);
+        piecesCtx.drawImage(piece.img, piece.x * squareSize + startPos, piece.y * squareSize + startPos, squareSize, squareSize);
       }
     }
   }
 }
 
 function drawBoard() {
-  ctx.fillStyle = colors[theme].padding;
-  ctx.fillRect(0, 0, size, size);
-  ctx.fillStyle = colors[theme].black;
-  ctx.fillRect(0 + startPos, 0 + startPos, size - squareSize, size - squareSize);
-  ctx.fillStyle = colors[theme].white;
+  bgCtx.fillStyle = themes[theme].padding;
+  bgCtx.fillRect(0, 0, size, size);
+  bgCtx.fillStyle = themes[theme].black;
+  bgCtx.fillRect(0 + startPos, 0 + startPos, size - squareSize, size - squareSize);
+  bgCtx.fillStyle = themes[theme].white;
   for (let i = 0; i < 8; i += 2) {
     for (let j = 0; j < 8; j += 2) {
-      ctx.fillRect(i * squareSize + startPos, j * squareSize + startPos, squareSize, squareSize);
-      ctx.fillRect((1 + i) * squareSize + startPos, (1 + j) * squareSize + startPos, squareSize, squareSize);
+      bgCtx.fillRect(i * squareSize + startPos, j * squareSize + startPos, squareSize, squareSize);
+      bgCtx.fillRect((1 + i) * squareSize + startPos, (1 + j) * squareSize + startPos, squareSize, squareSize);
     }
   }
-  ctx.strokeStyle = colors.line;
-  ctx.lineWidth = 2.5;
+  bgCtx.strokeStyle = themes.line;
+  bgCtx.lineWidth = 2.5;
   for (let i = 0; i <= 8; i++) {
-    ctx.beginPath();
-    ctx.moveTo(i * squareSize + startPos, 0 + startPos);
-    ctx.lineTo(i * squareSize + startPos, size - startPos);
-    ctx.stroke();
-    ctx.beginPath();
-    ctx.moveTo(0 + startPos, i * squareSize + startPos);
-    ctx.lineTo(size - startPos, i * squareSize + startPos);
-    ctx.stroke();
+    bgCtx.beginPath();
+    bgCtx.moveTo(i * squareSize + startPos, 0 + startPos);
+    bgCtx.lineTo(i * squareSize + startPos, size - startPos);
+    bgCtx.stroke();
+    bgCtx.beginPath();
+    bgCtx.moveTo(0 + startPos, i * squareSize + startPos);
+    bgCtx.lineTo(size - startPos, i * squareSize + startPos);
+    bgCtx.stroke();
 
-    ctx.fillStyle = "black";
-    ctx.textAlign = "center";
-    ctx.textBaseline = "middle";
-    ctx.font = squareSize / 5 + "px Verdana";
-    ctx.fillText("abcdefgh".charAt(i), squareSize + i * squareSize, size - startPos / 2);
-    ctx.fillText("12345678".charAt(i), startPos / 2, size - squareSize * (i + 1));
+    bgCtx.fillStyle = "black";
+    bgCtx.textAlign = "center";
+    bgCtx.textBaseline = "middle";
+    bgCtx.font = squareSize / 5 + "px Verdana";
+    bgCtx.fillText("abcdefgh".charAt(i), squareSize + i * squareSize, size - startPos / 2);
+    bgCtx.fillText("12345678".charAt(i), startPos / 2, size - squareSize * (i + 1));
   }
 }
 
@@ -108,26 +118,35 @@ function loadImage(src) {
     image.addEventListener("load", () => {
       resolve(image);
     });
-    image.src = `./img/${src}.svg`;
+    // image.src = `./img/${src}.svg`;
+    // image.src = `https://images.chesscomfiles.com/chess-themes/pieces/neo/150/${src}.png`;
+    // image.src = `https://images.chesscomfiles.com/chess-themes/pieces/game_room/150/${src}.png`;
+    image.src = `https://images.chesscomfiles.com/chess-themes/pieces/tournament/150/${src}.png`;
   });
 }
 
-canvas3.addEventListener("mousemove", (evt) => {
+animCanvas.addEventListener("mousemove", (evt) => {
   const rect = canvas.getBoundingClientRect();
   mouse.x = evt.clientX - rect.left - 5;
   mouse.y = evt.clientY - rect.top - 5;
 
-  ctx2.clearRect(0, 0, size, size);
-  if (!mouse.dragging) {
-    ctx2.fillStyle = "#0088ff55";
-  } else {
-    ctx2.fillStyle = "#00ff8855";
-  }
+  let coords = {
+    x: Math.floor((mouse.x - startPos) / squareSize),
+    y: Math.floor((mouse.y - startPos) / squareSize),
+  };
+
+  guideCtx.clearRect(0, 0, size, size);
+  guideCtx.fillStyle = "#0088ff55";
   if (mouse.x > startPos && mouse.y > startPos && mouse.x < size - startPos && mouse.y < size - startPos) {
     outerLoop: for (let x = startPos; x < size - squareSize; x += squareSize) {
       for (let y = startPos; y < size - squareSize; y += squareSize) {
         if (mouse.x > x && mouse.x < x + squareSize && (mouse.y > y) & (mouse.y < y + squareSize)) {
-          ctx2.fillRect(x, y, squareSize, squareSize);
+          guideCtx.fillRect(x, y, squareSize, squareSize);
+          if (board.pieces[coords.x][coords.y]) {
+            animCanvas.style.cursor = "grab";
+          } else {
+            animCanvas.style.cursor = "default";
+          }
           break outerLoop;
         }
       }
@@ -135,59 +154,102 @@ canvas3.addEventListener("mousemove", (evt) => {
   }
 
   if (mouse.dragging) {
-    canvas3.style.cursor = "none";
-    ctx2.drawImage(mouse.piece.img, mouse.x + mouse.offSet.x, mouse.y + mouse.offSet.y, squareSize, squareSize);
+    findLegalMoves(mouse.piece);
+
+    animCanvas.style.cursor = "grabbing";
+    animCtx.clearRect(0, 0, size, size);
+    animCtx.drawImage(mouse.piece.img, mouse.x - startPos, mouse.y - startPos, squareSize, squareSize);
   }
 });
 
-canvas3.addEventListener("mousedown", (evt) => {
+animCanvas.addEventListener("mousedown", (evt) => {
   let coords = {
     x: Math.floor((mouse.x - startPos) / squareSize),
     y: Math.floor((mouse.y - startPos) / squareSize),
   };
 
+  // Grabbing a piece
   if (board.pieces[coords.x][coords.y]) {
     let tmpPiece = board.pieces[coords.x][coords.y];
     mouse.dragging = true;
-    mouse.piece = new ChessPiece(tmpPiece.name, tmpPiece.color, tmpPiece.img, tmpPiece.x, tmpPiece.y);
-    mouse.offSet = {
-      x: coords.x * squareSize - mouse.x + startPos,
-      y: coords.y * squareSize - mouse.y + startPos,
-    };
-    console.log("You clicked on a piece");
-    console.log(board.pieces[coords.x][coords.y]);
-    console.log(mouse);
-    delete board.pieces[coords.x][coords.y];
-    ctx3.clearRect(startPos + coords.x * squareSize, startPos + coords.y * squareSize, squareSize, squareSize);
-  }
+    mouse.piece = new ChessPiece(tmpPiece.type, tmpPiece.color, tmpPiece.img, tmpPiece.x, tmpPiece.y);
 
-  if (!mouse.dragging) {
-    console.log("You didn't click on a piece");
+    delete board.pieces[coords.x][coords.y];
+    piecesCtx.clearRect(startPos + coords.x * squareSize, startPos + coords.y * squareSize, squareSize, squareSize);
+    animCtx.drawImage(mouse.piece.img, mouse.x - startPos, mouse.y - startPos, squareSize, squareSize);
+    animCanvas.style.cursor = "none";
+
+    findLegalMoves(mouse.piece);
   }
 });
 
-canvas3.addEventListener("mouseup", (evt) => {
+animCanvas.addEventListener("mouseup", (evt) => {
   if (mouse.dragging) {
     let coords = {
       x: Math.floor((mouse.x - startPos) / squareSize),
       y: Math.floor((mouse.y - startPos) / squareSize),
     };
     if (board.pieces[coords.x][coords.y]) {
+      // Square has a piece on it
       if (board.pieces[coords.x][coords.y].color !== mouse.piece.color) {
-        ctx3.clearRect(coords.x * squareSize + startPos, coords.x * squareSize + startPos, squareSize, squareSize);
-        ctx2.clearRect(coords.x * squareSize + startPos, coords.x * squareSize + startPos, squareSize, squareSize);
-        ctx3.drawImage(mouse.piece.img, coords.x * squareSize + startPos, coords.y * squareSize + startPos, squareSize, squareSize);
-        board.pieces[coords.x][coords.y] = new ChessPiece(mouse.piece.name, mouse.piece.color, mouse.piece.img, coords.x, coords.y);
+        // Square is occupied by the opponent
+        piecesCtx.clearRect(coords.x * squareSize + startPos, coords.y * squareSize + startPos, squareSize, squareSize);
+        guideCtx.clearRect(coords.x * squareSize + startPos, coords.y * squareSize + startPos, squareSize, squareSize);
+        piecesCtx.drawImage(mouse.piece.img, coords.x * squareSize + startPos, coords.y * squareSize + startPos, squareSize, squareSize);
+        board.pieces[coords.x][coords.y] = new ChessPiece(mouse.piece.type, mouse.piece.color, mouse.piece.img, coords.x, coords.y);
       } else {
-        ctx3.drawImage(mouse.piece.img, mouse.piece.x * squareSize + startPos, mouse.piece.y * squareSize + startPos, squareSize, squareSize);
-        board.pieces[mouse.piece.x][mouse.piece.y] = new ChessPiece(mouse.piece.name, mouse.piece.color, mouse.piece.img, mouse.piece.x, mouse.piece.y);
+        // Square is occupied by own piece
+        piecesCtx.drawImage(mouse.piece.img, mouse.piece.x * squareSize + startPos, mouse.piece.y * squareSize + startPos, squareSize, squareSize);
+        board.pieces[mouse.piece.x][mouse.piece.y] = new ChessPiece(mouse.piece.type, mouse.piece.color, mouse.piece.img, mouse.piece.x, mouse.piece.y);
       }
     } else {
-      board.pieces[coords.x][coords.y] = new ChessPiece(mouse.piece.name, mouse.piece.color, mouse.piece.img, coords.x, coords.y);
-      ctx3.drawImage(mouse.piece.img, coords.x * squareSize + startPos, coords.y * squareSize + startPos, squareSize, squareSize);
+      // Square is empty
+      board.pieces[coords.x][coords.y] = new ChessPiece(mouse.piece.type, mouse.piece.color, mouse.piece.img, coords.x, coords.y);
+      piecesCtx.drawImage(mouse.piece.img, coords.x * squareSize + startPos, coords.y * squareSize + startPos, squareSize, squareSize);
     }
+    animCtx.clearRect(0, 0, size, size);
     delete mouse.piece;
     mouse.dragging = false;
-    canvas3.style.cursor = "default";
+    animCanvas.style.cursor = "default";
   }
 });
+
+function findLegalMoves(piece) {
+  guideCtx.save();
+  let lineOffset = bgCtx.lineWidth;
+  guideCtx.lineWidth = bgCtx.lineWidth * 1.5;
+  let legalMoves = [];
+  for (let i = 0; i < piece.moves.length; i++) {
+    let x = piece.x;
+    let y = piece.y;
+    let repeat = piece.moves[i].repeat || false;
+
+    // If repeat...
+    do {
+      x += piece.moves[i].x || 0;
+      y += piece.moves[i].y || 0;
+      if (x >= 0 && x <= 7 && y >= 0 && y <= 7) {
+        if (board.pieces[x][y]) {
+          repeat = false;
+          if (board.pieces[x][y].color !== piece.color) {
+            legalMoves.push({ x, y });
+            guideCtx.strokeStyle = "#ff5555";
+            guideCtx.strokeRect(startPos + x * squareSize + lineOffset, startPos + y * squareSize + lineOffset, squareSize - lineOffset * 2, squareSize - lineOffset * 2);
+          }
+        } else {
+          legalMoves.push({ x, y });
+          guideCtx.fillStyle = "rgba(0, 0, 0, 0.25)";
+          guideCtx.beginPath();
+          guideCtx.arc(x * squareSize + squareSize, y * squareSize + squareSize, squareSize * 0.2, 0, Math.PI * 2);
+          guideCtx.fill();
+        }
+      } else {
+        repeat = false;
+      }
+    } while (repeat);
+
+    // End if repeat
+  }
+  guideCtx.restore();
+  return legalMoves;
+}
--- public/chess/js/classes.js
+++ public/chess/js/classes.js
@@ -3,12 +3,28 @@ export class Player {
 }
 
 export class ChessPiece {
-  constructor(name, color, img, posX, posY) {
-    this.name = name;
+  constructor(type, color, img, posX, posY) {
+    this.type = type;
     this.img = img;
     this.x = posX;
     this.y = posY;
     this.color = color;
+    this.hasMoved = false;
+    if (type !== "pawn") {
+      this.moves = moves[this.type];
+    } else {
+      if (color === "white") {
+        this.moves = [{ y: -1 }, { y: -2 }];
+      } else {
+        this.moves = [{ y: 1 }, { y: 2 }];
+      }
+    }
+  }
+
+  move(to) {
+    this.hasMoved = true;
+    this.x = to.x;
+    this.y = to.y;
   }
 }
 
@@ -26,42 +42,100 @@ function make2dArray(cols, rows) {
   return arr;
 }
 
-const names = ["rook1", "knight1", "bishop1", "queen", "king", "bishop2", "knight2", "rook2", "pawn1", "pawn2", "pawn3", "pawn4", "pawn5", "pawn6", "pawn7", "pawn8"];
-
-export const black = {
-  rook1: { x: 0, y: 0, imgName: "BR" },
-  knight1: { x: 1, y: 0, imgName: "BN" },
-  bishop1: { x: 2, y: 0, imgName: "BB" },
-  queen: { x: 3, y: 0, imgName: "BQ" },
-  king: { x: 4, y: 0, imgName: "BK" },
-  bishop2: { x: 5, y: 0, imgName: "BB" },
-  knight2: { x: 6, y: 0, imgName: "BN" },
-  rook2: { x: 7, y: 0, imgName: "BR" },
-  pawn1: { x: 0, y: 1, imgName: "BP" },
-  pawn2: { x: 1, y: 1, imgName: "BP" },
-  pawn3: { x: 2, y: 1, imgName: "BP" },
-  pawn4: { x: 3, y: 1, imgName: "BP" },
-  pawn5: { x: 4, y: 1, imgName: "BP" },
-  pawn6: { x: 5, y: 1, imgName: "BP" },
-  pawn7: { x: 6, y: 1, imgName: "BP" },
-  pawn8: { x: 7, y: 1, imgName: "BP" },
+const moves = {
+  rook: [
+    { x: 1, repeat: true },
+    { x: -1, repeat: true },
+    { y: 1, repeat: true },
+    { y: -1, repeat: true },
+  ],
+  knight: [
+    { x: 2, y: 1 },
+    { x: 2, y: -1 },
+    { x: -2, y: 1 },
+    { x: -2, y: -1 },
+    { x: 1, y: 2 },
+    { x: 1, y: -2 },
+    { x: -1, y: 2 },
+    { x: -1, y: -2 },
+  ],
+  bishop: [
+    { x: 1, y: 1, repeat: true },
+    { x: 1, y: -1, repeat: true },
+    { x: -1, y: 1, repeat: true },
+    { x: -1, y: -1, repeat: true },
+  ],
+  queen: [
+    { x: 1, repeat: true },
+    { x: -1, repeat: true },
+    { y: 1, repeat: true },
+    { y: -1, repeat: true },
+    { x: 1, y: 1, repeat: true },
+    { x: 1, y: -1, repeat: true },
+    { x: -1, y: 1, repeat: true },
+    { x: -1, y: -1, repeat: true },
+  ],
+  king: [{ x: 1 }, { x: -1 }, { y: 1 }, { y: -1 }, { x: 1, y: 1 }, { x: 1, y: -1 }, { x: -1, y: 1 }, { x: -1, y: -1 }],
 };
 
-export const white = {
-  rook1: { x: 0, y: 7, imgName: "WR" },
-  knight1: { x: 1, y: 7, imgName: "WN" },
-  bishop1: { x: 2, y: 7, imgName: "WB" },
-  queen: { x: 3, y: 7, imgName: "WQ" },
-  king: { x: 4, y: 7, imgName: "WK" },
-  bishop2: { x: 5, y: 7, imgName: "WB" },
-  knight2: { x: 6, y: 7, imgName: "WN" },
-  rook2: { x: 7, y: 7, imgName: "WR" },
-  pawn1: { x: 0, y: 6, imgName: "WP" },
-  pawn2: { x: 1, y: 6, imgName: "WP" },
-  pawn3: { x: 2, y: 6, imgName: "WP" },
-  pawn4: { x: 3, y: 6, imgName: "WP" },
-  pawn5: { x: 4, y: 6, imgName: "WP" },
-  pawn6: { x: 5, y: 6, imgName: "WP" },
-  pawn7: { x: 6, y: 6, imgName: "WP" },
-  pawn8: { x: 7, y: 6, imgName: "WP" },
-};
+export const black = [
+  { x: 0, y: 0, imgName: "br", type: "rook" },
+  { x: 1, y: 0, imgName: "bn", type: "knight" },
+  { x: 2, y: 0, imgName: "bb", type: "bishop" },
+  { x: 3, y: 0, imgName: "bq", type: "queen" },
+  { x: 4, y: 0, imgName: "bk", type: "king" },
+  { x: 5, y: 0, imgName: "bb", type: "bishop" },
+  { x: 6, y: 0, imgName: "bn", type: "knight" },
+  { x: 7, y: 0, imgName: "br", type: "rook" },
+  { x: 0, y: 1, imgName: "bp", type: "pawn" },
+  { x: 1, y: 1, imgName: "bp", type: "pawn" },
+  { x: 2, y: 1, imgName: "bp", type: "pawn" },
+  { x: 3, y: 1, imgName: "bp", type: "pawn" },
+  { x: 4, y: 1, imgName: "bp", type: "pawn" },
+  { x: 5, y: 1, imgName: "bp", type: "pawn" },
+  { x: 6, y: 1, imgName: "bp", type: "pawn" },
+  { x: 7, y: 1, imgName: "bp", type: "pawn" },
+];
+
+export const white = [
+  { x: 0, y: 7, imgName: "wr", type: "rook" },
+  { x: 1, y: 7, imgName: "wn", type: "knight" },
+  { x: 2, y: 7, imgName: "wb", type: "bishop" },
+  { x: 3, y: 7, imgName: "wq", type: "queen" },
+  { x: 4, y: 7, imgName: "wk", type: "king" },
+  { x: 5, y: 7, imgName: "wb", type: "bishop" },
+  { x: 6, y: 7, imgName: "wn", type: "knight" },
+  { x: 7, y: 7, imgName: "wr", type: "rook" },
+  { x: 0, y: 6, imgName: "wp", type: "pawn" },
+  { x: 1, y: 6, imgName: "wp", type: "pawn" },
+  { x: 2, y: 6, imgName: "wp", type: "pawn" },
+  { x: 3, y: 6, imgName: "wp", type: "pawn" },
+  { x: 4, y: 6, imgName: "wp", type: "pawn" },
+  { x: 5, y: 6, imgName: "wp", type: "pawn" },
+  { x: 6, y: 6, imgName: "wp", type: "pawn" },
+  { x: 7, y: 6, imgName: "wp", type: "pawn" },
+];
+
+/*
+
+Values:
+-----------
+Pawn: 1
+Bishop: 3
+Knight: 3
+Rook: 5
+Queen: 9
+
+
+*/
+
+let test = [
+  ["a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8"],
+  ["a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7"],
+  ["a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6"],
+  ["a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5"],
+  ["a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4"],
+  ["a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3"],
+  ["a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2"],
+  ["a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1"],
+];
